a9d455e82c034174fa277b2abde8eb4670433efc
11Olya112 <OlyaBushovska767@gmail.com>
2023-05-15 22:24:58 +0300

    add task solution

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: "https://github.com/11Olya112/vue3-test-designn/",
+  publicPath: "https://bogdana18.github.io/softpro_TodoList/",
   plugins: [vue()],
   resolve: {
     alias: {
