0d39e6b9c763cab38800911fbe918777228beba7
11Olya112 <OlyaBushovska767@gmail.com>
2023-05-15 21:04:15 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: "https://bogdana18.github.io/softpro_TodoList/",
+  publicPath: "https://github.com/11Olya112/vue3-test-designn/",
   plugins: [vue()],
   resolve: {
     alias: {
